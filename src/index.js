ec9aa54c21f6b27cf914dad2a095c40176dc2115
Yuv sinha <yuvsinha01@gmail.com>
2019-05-29 11:41:21 +0530

    Class based clock

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Myclass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            date : new Date()
+        }
+    }
 
+    componentDidMount(){
+        setInterval(this.My,1000);
+    }
 
-function MyFunction(props){
-    return <h1>HELLO {props.name} @ {props.at}</h1>;
+    My =()=>{
+        this.setState({
+            date : new Date()
+        })
+    }
+
+    render(){
+        return <h1> Time is: {this.state.date.toLocaleTimeString()}</h1>
+    }
 }
 
-
-function Show(){
-    return <div>
-     <MyFunction name='abc' at='Rpr'/>
-    <MyFunction name='def' at='Krar'/>
-    </div>
-        }
-
-ReactDOM.render(    
-   < Show/>
-, document.getElementById('root')    
+ReactDOM.render(
+    <Myclass />,
+    document.getElementById('root')
 );
