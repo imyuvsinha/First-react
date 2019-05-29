344e4631b038ab50b302ff1f10d95e637a8367cf
Yuv sinha <yuvsinha01@gmail.com>
2019-05-29 17:28:06 +0530

    create list item

--- src/index.js
+++ src/index.js
@@ -1,41 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-function Change(props){
-    if (props.value){
-        return <h1> This is the first one </h1>
-    }
-    return <h1>This is 2nd message</h1>
-}
-
-class Btn extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            value : true
-        
-        }
-    }
-
-    handler =()=>{
-        this.setState({ value: !this.state.value
-
-        });
-    }
-
-
-render() {
-    return (
-        <div>
-        <Change value={this.state.value} />
-        <button onClick={this.handler}>Change the message</button>
-       
-        </div>
-    )}
-}
+const name = ['abc','def','geh','abc']
+const car= name.map((name) => <li>{name}</li>)
 
 ReactDOM.render(
-    <Btn/>,
+    <ul>{car}</ul>,
     document.getElementById('root')
     
 );
