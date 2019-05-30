a7c168ab15916b2ced41103f7b2265188ac9c7c5
Yuv sinha <yuvsinha01@gmail.com>
2019-05-30 15:47:42 +0530

    conversion temperature

--- src/index.js
+++ src/index.js
@@ -1,34 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+ 
 
-class Forms extends React.Component{
+class Calculate extends React.Component{
     constructor(props){
         super(props)
-        this.state={ value:'ABC'
+        this.state={
+            scale:'c',
+            temp : 0
+    }
+    }
 
-        }
-    }
-    handleSubmit =(e) =>{
-        console.log(this.state.value)
-        e.preventDefault()
-    }
-    changer=(e)=>{
-        console.log(e.target.value)
+    
+    handleCelsius=(e)=>{
+
         this.setState({
-
-            value : !this.state.value
+            scale:'c',
+            temp : e.target.value
+            
+        })
+        console.log("abcd");
+    }
+    handleFahren=(e)=>{
+        this.setState({
+            scale:'f',
+            temp: e.target.value
         })
     }
+    render(){
+        const  temp  = this.state.temp;
+        const scale  = this.state.scale;
+        const cels = scale === 'f'? (temp-32)* 5/9 : temp;
+        const farh = scale === 'c'? (temp*9/5) + 32 : temp;
+        return (
+            
+            <div>
+                 <Main /> <br></br>
+                <fieldset>
+                    <legend>Scale Celsius</legend>
+                    <input value={cels} onChange={this.handleCelsius}/>
+                </fieldset>
 
-render(){
-    return (
-    <form onSubmit={this.handleSubmit}>
-      <select value={this.state.value} onChange={this.changer}>
-          <option value='abc'>abc</option>
-          <option value='ABC'>ABC</option>
-      </select>
-        <input type = "submit" value="Go ahead" />
-    </form>)
-        }
+                <fieldset>
+                    <legend>Scale Fahrenheit</legend>
+                    <input value={farh} onChange={this.handleFahren}/>
+                </fieldset>
+               
+            </div>
+        )
     }
-ReactDOM.render(<Forms/>, document.getElementById('root'));
+}
+
+const Main = () => (
+    <MuiThemeProvider>
+      <AppBar title="My Calculator" />
+    </MuiThemeProvider>
+  );
+  
+
+ReactDOM.render(<Calculate/>, document.getElementById('root'));
