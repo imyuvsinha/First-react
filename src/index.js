7a37a240bef18be49594ba318ddd7c0fb3c997a3
Yuv sinha <yuvsinha01@gmail.com>
2019-05-30 11:16:58 +0530

    location with console-timer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const name = ['abc','def','geh','abc']
-const car= name.map((name) => <li>{name}</li>)
 
-ReactDOM.render(
-    <ul>{car}</ul>,
-    document.getElementById('root')
-    
-);
+class X extends React.Component {
+    componentWillMount() {
+        console.time('willmount')
+        navigator.geolocation.getCurrentPosition(pos => console.log(pos));
+        console.timeEnd('willmount')
+    }
+    render() {
+        return <div>'Hello'</div>;
+    }
+}
+
+ReactDOM.render(<X/>, document.getElementById('root'));
