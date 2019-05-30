a041eaa580e9c8f5662812f28c9d52eff8d07292
Yuv sinha <yuvsinha01@gmail.com>
2019-05-30 12:26:56 +0530

    form handling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class X extends React.Component {
-    componentWillMount() {
-        console.time('willmount')
-        navigator.geolocation.getCurrentPosition(pos => console.log(pos));
-        console.timeEnd('willmount')
-    }
-    render() {
-        return <div>'Hello'</div>;
-    }
-}
+class Forms extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={ value:'ABC'
 
-ReactDOM.render(<X/>, document.getElementById('root'));
+        }
+    }
+    handleSubmit =(e) =>{
+        console.log(this.state.value)
+        e.preventDefault()
+    }
+    changer=(e)=>{
+        console.log(e.target.value)
+        this.setState({
+
+            value : !this.state.value
+        })
+    }
+
+render(){
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <select value={this.state.value} onChange={this.changer}>
+          <option value='abc'>abc</option>
+          <option value='ABC'>ABC</option>
+      </select>
+        <input type = "submit" value="Go ahead" />
+    </form>)
+        }
+    }
+ReactDOM.render(<Forms/>, document.getElementById('root'));
